3f15c198428b915af0cda4a9c5d5d01ac294f355
Aristevao <aristevao@hotmail.com>
2022-11-12 17:22:27 -0300

    Add definition comments

--- src/index.php
+++ src/index.php
@@ -2,14 +2,18 @@
 
 include_once "services/frete.php";
 
+// ---- Client ----
+// The client code picks a concrete strategy and passes it to
+// the context. The client should be aware of the differences
+// between strategies in order to make the right choice.
 $sedex = new \Services\Sedex();
 $dhl = new \Services\DHL();
 $me = new \Services\MercadoEnvio();
 
-$frete = new \Services\Frete($sedex);
+$frete = new \Services\Frete($sedex); // Pick SEDEX by constructor.
 echo $frete->calcula(10);
 
-$frete->setServico($dhl);
+$frete->setServico($dhl); // Pick DHL by setter.
 echo '<br>' . $frete->calcula(10);
 
 $frete->setServico($me);
--- src/services/frete.php
+++ src/services/frete.php
@@ -10,92 +10,82 @@ include_once "shipping/jadlog.php";
 include_once "shipping/tnt.php";
 include_once "shipping/mercadoenvio.php";
 
-// Strategy Interface
-interface FreteServico
-{
+// ---- Strategy Interface ----
+// The strategy interface declares operations common to all
+// supported versions of some algorithm. The context uses this
+// interface to call the algorithm defined by the concrete
+// strategies.
+interface FreteServico {
     function calcula(float $peso): float;
 }
 
-// Concrete Strategy - classes que implementam a interface
-class Sedex implements FreteServico
-{
-    function calcula(float $peso): float
-    {
+// ---- Concrete Strategy ----
+// Concrete strategies implement the algorithm while following
+// the base strategy interface. The interface makes them
+// interchangeable in the context.
+class Sedex implements FreteServico {
+    function calcula(float $peso): float {
         $correios = new \Correios();
         $valorTotal = $correios->calculaRemessa("SEDEX", $peso);
         return $valorTotal;
     }
 }
 
-class PAC implements FreteServico
-{
-    function calcula(float $peso): float
-    {
+class PAC implements FreteServico {
+    function calcula(float $peso): float {
         $correios = new \Correios();
         $valorTotal = $correios->calculaRemessa("PAC", $peso);
         return $valorTotal;
     }
 }
 
-class JadLog implements FreteServico
-{
-    function calcula(float $peso): float
-    {
+class JadLog implements FreteServico {
+    function calcula(float $peso): float {
         return  calculaFreteJadLog($peso);
     }
 }
 
-class DHL implements FreteServico
-{
-    function calcula(float $peso): float
-    {
+class DHL implements FreteServico {
+    function calcula(float $peso): float {
         $dhl = new \DHL();
         $valorTotal = $dhl->priceCalculator($peso);
         return $valorTotal;
     }
 }
 
-class Fedex implements FreteServico
-{
-    function calcula(float $peso): float
-    {
+class Fedex implements FreteServico {
+    function calcula(float $peso): float {
         $dhl = new \DHL();
         $valorTotal = $dhl->priceCalculator($peso);
         return $valorTotal;
     }
 }
 
-class TNT implements FreteServico
-{
-    function calcula(float $peso): float
-    {
+class TNT implements FreteServico {
+    function calcula(float $peso): float {
         $tnt = new \TNT();
         $valorTotal = $tnt->shippingPriceCalculator("PAC", $peso);
         return $valorTotal;
     }
 }
-class MercadoEnvio implements FreteServico
-{
-    function calcula(float $peso): float
-    {
+class MercadoEnvio implements FreteServico {
+    function calcula(float $peso): float {
         $tnt = new \MercadoEnvio();
         $valorTotal = $tnt->calcula($peso);
         return $valorTotal;
     }
 }
 
-// Context - consumo das classes Concretes
-class Frete
-{
+// ---- Context ----
+// The context defines the interface of interest to clients.
+class Frete {
     private $servico;
 
-    function __construct(FreteServico $servico)
-    {
+    function __construct(FreteServico $servico) {
         $this->servico = $servico;
     }
 
-    public function calcula(float $peso)
-    {
+    public function calcula(float $peso) {
         $valorTotal = $this->servico->calcula($peso);
         return $valorTotal;
     }
